cc71e627daaf8c20fbef3969d82203aa5c3c78e3
Shushan <noname320@hotmail.com>
2016-11-02 18:29:48 -0400

    Changed variable names so it would be coherent with the explanation, as well as changed main() for more clear operations.

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,26 @@ public class Main
 	public static void main(String[] args)
 	{
 		int n = 10;
-		for(int i = 0; i < 10; i++)
-			System.out.println("i = " + i + " : " + nbIterations(n, i-1) + " x " + (double)nbIterations(n, i)/nbIterations(n, i-1) + " = " + nbIterations(n, i));
-		
-		
+		for (int p = 1; p < 20; p++){
+			System.out.println("Avec n = " + n + " et p = " + p + " : " + nbIterations(n, p));
+		}
 	}
-	public static long nbIterations(int n, int i)
+	
+	public static long nbIterations(int n, int p)
 	{
 			long total = 0;
-			for (int m = 0; m < n; m++)
-				total += y(m,i);
+			for (int k = 0; k < n; k++)
+				total += y(k,p);
 			return total;
 	}
-	public static int y(int n, int i)
+	
+	public static int y(int n, int p)
 	{
 		if (n > 0)
 		{
 			int result = 0;
-			for (int k = 0; k <= i; k++)
-				result += y(n-1, i-k);
+			for (int k = 0; k <= p; k++)
+				result += y(n-1, p-k);
 			return result;
 		}
 		return 1;
